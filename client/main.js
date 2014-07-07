2db47d0d9298257accbd07cc2f1c3259a43e6cd2
Marc Williams <marc.williams@newwave-technologies.com>
2014-07-07 10:52:00 -0400

    big update to fix UI issues from upgrad to Blaze

--- client/main.js
+++ client/main.js
@@ -4,236 +4,336 @@ if (Meteor.isClient) {
 
 Router.map(function() {
   this.route('home', {path: '/'});
+  
   this.route('userTable');
+  
   this.route('userBio', {template: 'userBio'});
-  this.route('profilePage', {path: '/userTable/:_id'});
+  
+  this.route('profilePage', {
+    path:   'profilePage/:_id',
+    action: function(){
+      this.render();
+    }
+  });
+  
 })
+ 
 
 Session.setDefault('updating_user', null);
-// Session.setDefault('myFilter', {completed: true, limit: 30});
 
-// Deps.autorun(function(){
-// 	var filter = Session.get('myFilter');
-// 	Meteor.subscribe('listUserFiles', filter);
-// })
-	Template.userTable.users = function () {
-		// listOfUsers = Meteor.call('returnAllUsers');
-		return Users.find({});
-	}
+  Template.userTable.users = function () {
+    return Users.find({});
+  }
 
-	Template.home.events({
-		'click #removeAll' : function(){
-		Meteor.call('removeAllUsers');
-		Router.go('userTable');
-			}
-		}
-	);
+  Template.home.events({
+    'click #removeAll' : function(){
+    Meteor.call('removeAllUsers');
+    Router.go('userTable');
+      }
+    }
+  );
 
-	Template.profilePage.oneGuy = function(){
-		return Users.findOne({_id: Session.get('updating_user')});
-	}
+  Template.profilePage.oneGuy = function(){
+    return Users.findOne({_id: Session.get('updating_user')});
+  }
 
-	Template.nameSpace.oneGuy = function(){
-		return Users.findOne({_id: Session.get('updating_user')});
-	}
+  Template.nameSpace.oneGuy = function(){
+    return Users.findOne({_id: Session.get('updating_user')});
+  }
 
-	Template.bankTeller.oneGuy = function(){
-		return Users.findOne({_id: Session.get('updating_user')});
-	}
+  Template.bankTeller.oneGuy = function(){
+    return Users.findOne({_id: Session.get('updating_user')});
+  }
 
-	Template.mailer.user = function(){
-		return Users.findOne({_id: Session.get('updating_user')});
-	}
+  Template.mailer.user = function(){
+    return Users.findOne({_id: Session.get('updating_user')});
+  }
 
-	Template.userTable.events({
-		'click #saveBtn': function(e){
-			var fName = $('#firstName').val();
-			var lName = $('#lastName').val();
-			var accBalance = accounting.formatColumn([$('#accountBalance').val()], "$ ");
-			var bio = $('#userBio').val();
-			e.preventDefault();
+  Template.userTable.events({
+    'click #saveBtn': function(e){
+      var fName = $('#firstName').val();
+      var lName = $('#lastName').val();
+      var accBalance = accounting.formatColumn([$('#accountBalance').val()], "$ ");
+      var bio = $('#userBio').val();
+      e.preventDefault();
 
-			if(Session.get('updating_user')){
-				updateUser(Session.get('updating_user'),fName, lName, accBalance, bio, "");
-			} else {
-				insertUser(fName, lName, accBalance, bio, "");				
-			}
+      if(Session.get('updating_user')){
+        updateUser(Session.get('updating_user'),fName, lName, accBalance, bio, "");
+      } else {
+        insertUser(fName, lName, accBalance, bio, "");        
+      }
 
-			$('#firstName').val('');
-			$('#lastName').val('');
-			$('#accountBalance').val('');
-			$('#userBio').val('');
+      $('#firstName').val('');
+      $('#lastName').val('');
+      $('#accountBalance').val('');
+      $('#userBio').val('');
 
-			Session.set('updating_user', null);
-		} ,
+      Session.set('updating_user', null);
+    } ,
 
-		'click #cleanSlate': function(){
-			Meteor.call('removeAllUsers');
-		} , 
+    'click #cleanSlate': function(){
 
-		'click .deleteUser': function(){
-			if(confirm('Are you sure you want to remove this user?')){
-				var user = Users.findOne({_id:this._id});
-				Users.remove(this._id);
-				Meteor.call('deleteUser', user.firstName, user.lastName);
-			}
-		} ,
-		
-		'click .viewBioPage' : function(){
-			Session.set('updating_user', this._id);
-			Router.go('profilePage');
-			Session.set('updating_user', null);
-		} ,
+    var clearCommand = "CLEAR";
+    var commandObj = {"command":clearCommand};
+    var message = JSON.stringify(commandObj);
 
-		'click .updateUser' : function(){
-			Session.set('updating_user', this._id);
-			var oneUser = Users.findOne({_id: this._id});
-			$('#firstName').val(oneUser.firstName);
-			$('#lastName').val(oneUser.lastName);
-			$('#accountBalance').val(accounting.unformat(oneUser.accountBalance));
-			$('#userBio').val(oneUser.userBio);
+    alert(message);
 
-		} ,
+    Meteor.call("useQueue", message);
 
-		'click #goHome' : function(){
-			Router.go('home');
-		} , 
 
-		'click #offlineMode' : function(){
-			if(confirm('Are you sure you want to disconnect from Meteor server? This will cease live updates')){
-				Meteor.call('mySQLDisconnect');
-			}
-			Meteor.disconnect();
-		} ,
 
-		'click #onlineMode' : function(){
-				Meteor.call('mySQLConnect');
-				Meteor.reconnect();
-				alert('You are now back online');
-		}
-	});
+
+      // Meteor.call('removeAllUsers');
+
+    } , 
+
+    'click .deleteUser': function(){
+      if(confirm('Are you sure you want to remove this user?')){
+        var user = Users.findOne({_id:this._id});
+        // var id = this._id;
+        // alert(id);
+        // removeUser(this._id);
+        removeUser(user);
+        Users.remove(this._id);
+      }
+    } ,
+    
+    'click .viewBioPage' : function(){
+      Session.set('updating_user', this._id);
+      // Router.go('profilePage');
+      // Router.go('home');
+      Session.set('updating_user', null);
+    } ,
+
+    'click .updateUser' : function(){
+      Session.set('updating_user', this._id);
+      var oneUser = Users.findOne({_id: this._id});
+      $('#firstName').val(oneUser.firstName);
+      $('#lastName').val(oneUser.lastName);
+      $('#accountBalance').val(accounting.unformat(oneUser.accountBalance));
+      $('#userBio').val(oneUser.userBio);
+
+    } ,
+
+    'click #goHome' : function(){
+      Router.go('home');
+    } , 
+
+    'click #offlineMode' : function(){
+      if(confirm('Are you sure you want to disconnect from Meteor server? This will cease live updates')){
+        Meteor.call('mySQLDisconnect');
+        Meteor.disconnect();
+      }
+      
+    } ,
+
+    'click #onlineMode' : function(){
+        
+      // Meteor.call('returnAllUsers', function(err, response){
+     //   var x = " " + response;
+     //   // alert("(from client) response is " + x[0].firstName);
+     //   console.log("--->>>>> " + x);
+     //   });
+     //   // var b = Session.get('allUsers');
+     //   // alert("b is " + b);
+      
+      // Meteor.reconnect();
+
+      // mysqlCatchUp(Users.find({}).fetch());
+
+      var message = "Hello World!";
+      Meteor.call('useQueue', message);
+
+        // Meteor.call('mySQLConnect');
+        // Meteor.reconnect();
+        // alert('You are now back online');
+    }
+  });
 
 Template.profilePage.events({
-	'click #displayName' : function(){
-		$('#displayArea').html( generateTemplate('nameSpace') );
-	} ,
+  'click #displayName' : function(){
+    $('#displayArea').html( generateTemplate('nameSpace') );
+  } ,
 
-	'click #displayBalance' : function(){
-		var user = Users.findOne({_id: this._id});
-		$('#displayArea').html( generateTemplate('bankTeller') );
-	} ,
+  'click #displayBalance' : function(){
+    var user = Users.findOne({_id: this._id});
+    $('#displayArea').html( generateTemplate('bankTeller') );
+  } ,
 
-	'click #displayBiography' : function(){
-		var user = Users.findOne({_id: this._id});
-		$('#displayArea').text(user.userBio);
-	} , 
+  'click #displayBiography' : function(){
+    var user = Users.findOne({_id: this._id});
+    $('#displayArea').text(user.userBio);
+  } , 
 
-	'click #displayEmail' : function(){
-		var user = Users.findOne({_id: this._id});
-		$('#displayArea').html( generateTemplate('mailer') );
-	} , 
+  'click #displayEmail' : function(){
+    var user = Users.findOne({_id: this._id});
+    $('#displayArea').html( generateTemplate('mailer') );
+  } , 
 
-	'click #tablePage' : function(){
-		Router.go('userTable');
-	} 
+  'click #tablePage' : function(){
+    Router.go('userTable');
+  } 
 });
 
 Template.nameSpace.events({
-	'click #moreBtn' : function(){
-		$('#moreInfoSection').toggle();
-	} ,
+  'click #moreBtn' : function(){
+    $('#moreInfoSection').toggle();
+  } ,
 
-	'submit #infoForm' : function(e){a
-		var input = $('#addInfo')[0].value;
-		var user = Users.findOne({_id:this._id});
-		alert("inside subit #infoForm -> " + this._id);
-		updateUser(this._id, user.firstName, user.lastName, user.accountBalance, user.userBio, input);
-		e.preventDefault();
-	}
+  'submit #infoForm' : function(e){a
+    var input = $('#addInfo')[0].value;
+    var user = Users.findOne({_id:this._id});
+    alert("inside subit #infoForm -> " + this._id);
+    updateUser(this._id, user.firstName, user.lastName, user.accountBalance, user.userBio, input);
+    e.preventDefault();
+  }
 });
 
-	Template.bankTeller.events({
-		'click #showATM' : function(){
-			$('#displayButtons').toggle();
-		} ,
+  Template.bankTeller.events({
+    'click #showATM' : function(){
+      $('#displayButtons').toggle();
+    } ,
 
-		'click #depositAmount' : function(e){
-		var user = Users.findOne({_id: this._id});
-		var userBalance = accounting.unformat(user.accountBalance);
-		var amount = $('#amount')[0].value;
-		var newBalance = accounting.formatColumn([+userBalance + +amount], "$ ");
-		updateUser(this._id, user.firstName, user.lastName, newBalance, user.userBio, user.moreInfo);
-		e.preventDefault();
-		} ,
+    'click #depositAmount' : function(e){
+    var user = Users.findOne({_id: this._id});
+    var userBalance = accounting.unformat(user.accountBalance);
+    var amount = $('#amount')[0].value;
+    var newBalance = accounting.formatColumn([+userBalance + +amount], "$ ");
+    updateUser(this._id, user.firstName, user.lastName, newBalance, user.userBio, user.moreInfo);
+    e.preventDefault();
+    } ,
 
-		'click #withdrawAmount' : function(e){
-		var user = Users.findOne({_id: this._id});
-		var userBalance = accounting.unformat(user.accountBalance);
-		var amount = $('#amount')[0].value;
-		var newBalance = accounting.formatColumn([+userBalance - +amount], "$ ");
-		
-		if(amount > userBalance){
-			if(confirm('If you withdraw ' + accounting.formatMoney(amount) + ' your account will be overdrawn. Are you sure want to continue?')){
-				updateUser(this._id, user.firstName, user.lastName, newBalance, user.userBio, user.moreInfo);
-				}
-			} else {
-				updateUser(this._id, user.firstName, user.lastName, newBalance, user.userBio, user.moreInfo);
-			}
-			e.preventDefault();
-		}
-	});
+    'click #withdrawAmount' : function(e){
+    var user = Users.findOne({_id: this._id});
+    var userBalance = accounting.unformat(user.accountBalance);
+    var amount = $('#amount')[0].value;
+    var newBalance = accounting.formatColumn([+userBalance - +amount], "$ ");
+    
+    if(amount > userBalance){
+      if(confirm('If you withdraw ' + accounting.formatMoney(amount) + ' your account will be overdrawn. Are you sure want to continue?')){
+        updateUser(this._id, user.firstName, user.lastName, newBalance, user.userBio, user.moreInfo);
+        }
+      } else {
+        updateUser(this._id, user.firstName, user.lastName, newBalance, user.userBio, user.moreInfo);
+      }
+      e.preventDefault();
+    }
+  });
 
-	Template.mailer.events({
-		'click #sendMessage' : function(){
-			var user = Users.findOne({_id: this._id});
-			var to = $('#toField').val();
-			var from = user.firstName + "." + user.lastName + "@meteormail.com"
-			var subject = $('#subjectField').val();
-			var text = $('#messageBox')[0].value;
-			
-			Meteor.call('sendEmail', to, from, subject, text);
-		}
-	})
+  Template.mailer.events({
+    'click #sendMessage' : function(){
+      var user = Users.findOne({_id: this._id});
+      var to = $('#toField').val();
+      var from = user.firstName + "." + user.lastName + "@meteormail.com"
+      var subject = $('#subjectField').val();
+      var text = $('#messageBox')[0].value;
+      
+      Meteor.call('sendEmail', to, from, subject, text);
+    }
+  })
 
-	var generateTemplate = function(templateName){
-		var fragment = Meteor.render(function() {
-			return Template[templateName]();
-		});
+  var generateTemplate = function(templateName){
+    var fragment = Meteor.render(function() {
+      return Template[templateName]();
+    });
 
-		return fragment;
-	}
+    return fragment;
+  }
 
-	var updateUser = function(id,fName, lName, accBalance, bio, info){
-		alert("user update?");
-		//needs to be completed
-		// Meteor.call('updateUsers');
-		alert("inside updateUser -> " + id);
-	Users.update(id, 
-		{$set: {firstName : fName,
-		 lastName : lName, 
-		 accountBalance : accBalance, 
-		 userBio : bio,
-		 moreInfo : info
-		}});
-	Meteor.call('updateUsers', id, fName, lName, accounting.unformat(accBalance), bio, info);
-	}
+  var removeUser = function(user){
+    // var x = id + "";
+    // alert("in dU id = " + x);
+    // alert("id is " + id);
+    // var user = Users.findOne({_id:id});
+    alert(user._id + "");
 
-	var insertUser = function(fName, lName, accBalance, bio, info) {		
-		var mysqlID = Users.insert({firstName: fName, 
-			lastName: lName, 
-			accountBalance: accBalance,
-			 userBio: bio,
-			 moreInfo : info
-			});
-			Meteor.call('logUsers', mysqlID, fName, lName, accounting.unformat(accBalance), bio, info);
-	}
+    var userID = user._id + "";
+    var unformattedMoney = accounting.unformat(user.accountBalance);
 
-	// var latestGuy = function(){
-	// 	var latestUser = Users.find({}, {sort: {_id : -1} , limit: 1}).fetch()[0];
-	// 	alert("latestUser id is " + latestUser._id);
+    var deleteCommand = "DELETE";
+    var placeHold = "";
+    var userObj = {"command":deleteCommand, "userID":userID, "firstName":user.firstName, "lastName":user.lastName, "accountBalance":unformattedMoney,"userBio":user.userBio,"moreInfo":user.moreInfo};
+    var message = JSON.stringify(userObj);
+
+    Meteor.call("useQueue", message);
+    
 
 
-	// 	return latestUser;
-	// 	// alert("Latest user is " + latestUser.fName + latestUser.lName + latestUser._id);
-	// }
+    // Meteor.call('deleteUser', x);
+
+  }
+
+  var updateUser = function(id,fName, lName, accBalance, bio, info){
+  Users.update(id, 
+    {$set: {firstName : fName,
+     lastName : lName, 
+     accountBalance : accBalance, 
+     userBio : bio,
+     moreInfo : info
+    }});
+  var mysqlID = id+ "";
+  alert("MYSQLID ~~~~~~~~~~ " + id + " ~~~~~~~~~~~~~~");
+
+  var unformattedMoney = accounting.unformat(accBalance);
+  var updateCommand = "UPDATE";
+  var userObj = {"command":updateCommand, "userID":mysqlID, "firstName":fName, "lastName":lName, "accountBalance":unformattedMoney,"userBio":bio,"moreInfo":info};
+
+  // alert(unformattedMoney + " <-----> " + updateCommand + " <-----> " + JSON.stringify(userObj));
+  var message = JSON.stringify(userObj);
+  Meteor.call('useQueue', message);
+
+  Meteor.call('updateUsers', id, fName, lName, accounting.unformat(accBalance), bio, info);
+  }
+
+
+  var insertUser = function(fName, lName, accBalance, bio, info) {    
+    var mysqlID = Users.insert({firstName: fName, 
+      lastName: lName, 
+      accountBalance: accBalance,
+       userBio: bio,
+       moreInfo : info
+      });
+
+    var unformattedMoney = accounting.unformat(accBalance);
+    mysqlID = mysqlID + "";
+    var createCommand = "CREATE";
+    var userObj = {"command":createCommand, "userID":mysqlID, "firstName":fName, "lastName":lName, "accountBalance":unformattedMoney,"userBio":bio,"moreInfo":info};
+    var message = JSON.stringify(userObj);
+    Meteor.call('useQueue', message);
+
+    alert("~~~~~~~~~~ " + mysqlID + " ~~~~~~~~~~~~~~");
+      Meteor.call('logUsers', mysqlID, fName, lName, unformattedMoney, bio, info);
+  }
+
+  var mysqlCatchUp = function (mongoArray){
+    alert("Catching Up... User count = " + mongoArray.length);
+
+    //reconnet to sql server
+    alert("Attempting to call mysqlConnect");
+    // Meteor.call('emptyMysqlTable');
+
+    Meteor.call('mySQLConnect', mongoArray); 
+    // for(var i = 0; i < mongoArray.length; i++){
+    //  var mysqlID = mongoArray[i]._id + "";
+    //  var firstName = mongoArray[i].firstName;
+    //  var lastName = mongoArray[i].lastName;
+    //  var accountBalance = mongoArray[i].accountBalance;
+    //  var bio = mongoArray[i].userBio;
+    //  var info = mongoArray[i].moreInfo;
+
+    //  // alert("USER INFO: " + mysqlID + " | " + firstName + " | " + lastName + " | " + accountBalance + " | " + bio + " | " + info);
+
+    //  Meteor.call('logUsers', mysqlID, firstName, lastName, accounting.unformat(accountBalance), bio, info);
+    // }
+  }
+
+  // var latestGuy = function(){
+  //  var latestUser = Users.find({}, {sort: {_id : -1} , limit: 1}).fetch()[0];
+  //  alert("latestUser id is " + latestUser._id);
+
+
+  //  return latestUser;
+  //  // alert("Latest user is " + latestUser.fName + latestUser.lName + latestUser._id);
+  // }
 }
--- server/main.js
+++ server/main.js
@@ -2,88 +2,159 @@ Meteor.publish('users', function() {
     return Users.find({});
 });
 
+
 var mysql = Meteor.require('mysql');
+var amqp = Meteor.require('amqp');
+
+// var amConn = amqp.createConnection({host: 'localhost'});
+var amConn = new amqp.Connection();
+amConn.connect();
 
 var db_config = {
 	host	: 'localhost',
 	user 	: 'root',
 	password: '',	
-	port 	: '3306'
+	port 	: '3306',
+	database: 'meteor_java',
 };
 
-
-startConnect = function(config){
-	var bridge = mysql.createConnection(config);
-	return bridge;
-}
-
-var connection = startConnect(db_config);
+var connection;
 
 
-mysql.createConnection(db_config);
+handleDisconnect = function(mongoArray){
+	console.log("handling disconnect...")
+	connection = mysql.createConnection(db_config);                                                
+	console.log("INSIDE handleDisconnect *********** " + mongoArray.length + " ***********");
+
+  	connection.connect(function(err) {              
+    if(err) {                                     
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); 
+    } else {
+    	console.log("~~~~~~~~~~~~ MySQL connection Restablished ~~~~~~~~~~~~~");
+    	clearTable();
+
+    		for(var i = 0; i < mongoArray.length; i++){
+			var mysqlID = mongoArray[i]._id + "";
+			var firstName = mongoArray[i].firstName;
+			var lastName = mongoArray[i].lastName;
+			var accountBalance = mongoArray[i].accountBalance;
+			var bio = mongoArray[i].userBio;
+			var info = mongoArray[i].moreInfo;
+
+			// alert("USER INFO: " + mysqlID + " | " + firstName + " | " + lastName + " | " + accountBalance + " | " + bio + " | " + info);
+
+			Meteor.call('logUsers', mysqlID, firstName, lastName, accounting.unformat(accountBalance), bio, info);
+		}
+
+    }                                    
+  });
+  }                                     
 
 Meteor.startup(function(){
 	Apm.connect('CjwQdohh83iq9dtWc', '2cfdc412-531b-490b-bb13-99befa9ab7fe');
+	
+	console.log("db_config -> " + db_config);
+	connection = mysql.createConnection(db_config);
+	
+// 	//receive 
+// amConn.on('ready', function(){
+//     amConn.queue('task_queue', {autoDelete: false, durable: true}, function(queue){
+
+//         console.log(' [*] Waiting for messages. To exit press CTRL+C');
+
+//         queue.subscribe({ack: true, prefetchCount: 1}, function(msg){
+//             var body = msg.data.toString('utf-8');
+//             console.log(" [x] Received { %s }", body);
+//             setTimeout(function(){
+//                 console.log(" [x] Done");
+//                 queue.shift(); // basic_ack equivalent
+//             }, (body.split('.').length - 1) * 1000);
+//         });
+//     });
+// });
+
+
+
 	Meteor.Mailgun.config({
 		username: 'postmaster@metoermailgun.com',
 		password: 'aliveWithHandz'
 	});
+	
 
-connection.connect(function(err){
-	console.log("---------- MySQL connection established ----------");
-});
+mysqlStartup = function(mysqlElement, db_config){
+	console.log(db_config);
+	var connection = mysql.createConnection(db_config);
+}
+
+
+
+
 connection.query('create schema if not exists meteor');
-connection.query('use meteor');
-var createUserTable = "CREATE TABLE IF NOT EXISTS Users(userID varchar(45), firstName VARCHAR(45) NULL, lastName VARCHAR(45) NULL, accountBalance DECIMAL NULL, userBio VARCHAR(45) NULL, info VARCHAR(45) NULL, PRIMARY KEY (userID), UNIQUE INDEX idUsers_UNIQUE (userID ASC))";
+var createUserTable = "CREATE TABLE IF NOT EXISTS meteor_user(userID varchar(45), firstName VARCHAR(45) NULL, lastName VARCHAR(45) NULL, accountBalance DECIMAL NULL, userBio VARCHAR(45) NULL, info VARCHAR(45) NULL, PRIMARY KEY (userID), UNIQUE INDEX idUsers_UNIQUE (userID ASC))";
 
 connection.query(createUserTable, function(err, result){
-	callback(result);
+	// callback(result);
 });
 
-var selectFromPersons = "select * from Users";
+var selectFromPersons = "SELECT * FROM meteor_user";
 connection.query(selectFromPersons, function(err, results){
-	console.log(results);
+	if(err){
+	} else{ 
+	console.log("++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+	console.dir(results);
+	console.log("++++++++++++++++++++++++++++++++++++++++++++++++++++++");	
+	}
+	
 });
 
 });
+
+//////////////////////////////////////////////////////////////////////// 
 	callback = function(x){
-		console.dir(x);
+		console.dir("Message from callback " + x);
+		for(var i = 0; i < x.length; i++){
+			console.dir(i + " -->>> " + x[i].userdID + " | " +  x[i].firstName + " | " +  x[i].lastName + " | " +  x[i].accountBalance + " | " +  x[i].userBio);
+		}
+
 	}
+//////////////////////////////////////////////////////////////////////// 
 
 	allUsers = function(client){
-		return client.query("SELECT * FROM Users");
+		return client.query("SELECT * FROM meteor_user");
 	}
 
 	clearTable = function(client){
-		console.log("clearing Users table")
-		client.query("DELETE FROM Users");
+		console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ clearing meteor_users table ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+		client.query("DELETE FROM meteor_user");
 	}
-	tableUpdate = function(client, id, fName, lName, accountBalance, userBio){
+	tableUpdate = function(client, id, fName, lName, accountBalance, userBio, userInfo){
 		console.log("updating user in MySQL...");
-
-		// client.query("UPDATE Users SET firstName = + '" + fName + "' AND SET lastName = '" + lName + "' WHERE userID = '" + id + "'");
-
-		client.query('UPDATE Users SET firstName = ? AND lastName = ? WHERE userID = ?', [fName, lName, id]);
+		var u = {fn : fName, ln : lName, ID : id, bal : accountBalance, bio : userBio, moreInfo : userInfo};
+		var x = id + "";
+		client.query('UPDATE meteor_user SET firstName = ? , lastName = ? , accountBalance = ? , userBio = ?, info = ? WHERE userID = ?', [u.fn, u.ln, u.bal, u.bio, u.moreInfo,  x]);
 
 		}
 
-	tableDelete = function(client, fName, lName){
-		client.query("DELETE from Users WHERE firstName = + '" + fName + "' AND lastName = '" + lName + "'");
-	}
+	mysqlReconnect = function(mongoArray){
+		console.log("INSIDE mysqlReconnect *********** " + mongoArray.length + " ***********");
+		handleDisconnect(mongoArray);
+		allUsers(connection);
+	}	
+
+	// tableDelete = function(client, id){
+	// 	var mysqlID = id + "";
+	// 	console.log("id to delete is " + mysqlID);
+	// 	client.query("DELETE from meteor_user WHERE userID = '" + id + "'");
+	// }
 
 	dropConnection = function(client){
+		client.end();
 		console.log("MySQL connection terminated");
-		client.destroy();
-	}
-
-	reconnection = function(client){
-		client.connect(function(err) {
-			console.log("MySQL connection established");
-		});
 	}
 
 	tableReady = function(client, id, fName, lName, accBalance, bio, info){
-		client.query("INSERT INTO Users (userID, firstName, lastName, accountBalance, userBio, info) VALUES ('" + id + "', '"+ fName + "', '"+ lName + "', '"+ accBalance + "', '"+ bio + "', '" + info + "')",
+		client.query("INSERT INTO meteor_user (userID, firstName, lastName, accountBalance, userBio, info) VALUES ('" + id + "', '"+ fName + "', '"+ lName + "', '"+ accBalance + "', '"+ bio + "', '" + info + "')",
 			function(err, results){
 				if(err) {
 					console.log("Error -> " + err.message);
@@ -98,36 +169,81 @@ connection.query(selectFromPersons, function(err, results){
 
 	Meteor.methods({
 		removeAllUsers : function() {
-			// console.log("Clearing Users....");
 			clearTable(connection);
 			return Users.remove({});
+		} , 
+
+		useQueue : function(message){
+			console.log("message on server side -> " + message);
+			var amConn = new amqp.Connection({host: 'localhost'});
+			amConn.connect(); 
+	
+		//send
+		amConn.on('ready', function(){
+    	amConn.queue('task_queue', {autoDelete: true, durable: true}, function(queue){
+        amConn.publish('task_queue', message, {deliveryMode: 2});
+        console.log(" [x] Sent { %s }", message);
+    });
+});
+
+
+
+	
+		} ,
+
+		emptyMysqlTable : function(){
+			clearTable(connection);
 		} ,
 
 		mySQLDisconnect : function(){
 			dropConnection(connection);
 		} ,
 
-		mySQLConnect : function(){
-			reconnection(connection);
+		mySQLConnect : function(mongoArray){
+			console.log("calling reconnection... ");
+			mysqlReconnect(mongoArray);
 		} ,
 
-		returnAllUsers : function(){
-			return allUsers(connection);
-		} ,
+		// returnAllUsers : function(){
+		// connection.query("SELECT * FROM Users", function(err, results){
+		// 	if(err){
+		// 		console.log("whoops -> " + err);
+		// 	} else {
+		// 		// console.log("The result is: " + results[0].firstName);
+		// 		 // results;
+		// 		 mon = results;
+		// 		 callback(results);
+		// 		 return mon;
+		// 	}
 
-		deleteUser: function(fName, lName){
-			console.log("firstName -> " + fName);
-			console.log("lastName -> " + lName);
-			tableDelete(connection, fName, lName);
-		} ,
+		// 	// json = JSON.stringify(results);
+		// 	// json = results;
+		// 	// console.log("THE JSON is --->>>>>> " + json);
+		// 	// return json;
+			
+		// // })
+		// // 	console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+		// // 	console.log("results - " + mon[0].firstName);
+		// // 	console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+		// // 	return mon[0];
+		// ;
+		// 	// return allUsers(connection);
+		// } ,
+
+		// deleteUser: function(id){
+		// 	console.log("from deleteUser id is " + id);
+		// 	tableDelete(connection, id);
+		// } ,
 
 		logUsers : function(id, fName, lName, accBalance, bio, info){
-			tableReady(connection, id, fName, lName, accBalance, bio, info);
+			// tableReady(connection, id, fName, lName, accBalance, bio, info);
 		} ,
 
 		updateUsers : function(id, fName, lName, accBalance, bio, info){
 			console.log("updating user...");
-			tableUpdate(connection, id, fName, lName, accBalance, bio, info);
+			console.log("~~~~~~~~~~ " + id + " ~~~~~~~~~~~~~~");
+
+			// tableUpdate(connection, id, fName, lName, accBalance, bio, info);
 		} ,
 
 		sendEmail : function(to, from, subject, text){
